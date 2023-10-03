c729ae21a72be8920b7123250ead9038fdca2987
Holly T <144973465+thompsonholly@users.noreply.github.com>
2023-10-02 21:37:36 -0500

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,46 @@
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
+var questions =
+var scoreboard = 0
+
+generateQuiz() {
+
+}
+
+function setTime() {
+
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage("Time's Up!");
+    }
+
+  }, 1000);
+}
+generateStartBtn.addEventListener("click", showScoreboard);
+
+function showScoreboard() {
+  scoreboard = generateQuiz();
+
+  var scoreboardResult = document.querySelector("#results");
+
+  scoreboard.valueOf(number + initials)
+}
+
+setTime();
